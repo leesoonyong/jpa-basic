0d4c6f8b0c7bfc44cfeeb669e7427806310a20ac
awa-intra1 <wnghkdcjdtns@naver.com>
2023-01-21 02:15:24 +0900

    JPA firstCommit

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,28 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = emf.createEntityManager();
+
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+        //code
+        try {
+            Member member = entityManager.find(Member.class, 1L);
+            member.setName("helloJPA");
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            entityManager.close();
+            emf.close();
+        }
+    }
+}
