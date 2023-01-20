1765161d0fe72427258936072a9164308b6341ec
awa-intra1 <wnghkdcjdtns@naver.com>
2023-01-21 02:35:17 +0900

    commit

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,9 +15,13 @@ public class JpaMain {
         tx.begin();
         //code
         try {
-            Member member = entityManager.find(Member.class, 1L);
-            member.setName("helloJPA");
-
+            List<Member> result = entityManager.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+            for (Member member1 : result) {
+                System.out.println("member = " + member1.getName());
+            }
             tx.commit();
         }catch (Exception e){
             tx.rollback();
