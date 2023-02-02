d04304b3c6c709bc1c525ba027b17a8c7709443e
awa-intra1 <wnghkdcjdtns@naver.com>
2023-02-02 17:56:37 +0900

    commit

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.hibernate.Hibernate;
 
 import javax.persistence.*;
@@ -15,12 +16,29 @@ public class JpaMain {
         //code
         try {
             Member member = new Member();
+            member.setUsername("kime");
+            member.setHomeAddress(new Address("11","22","33"));
 
-            member.setUsername("MEMBER1");
-            member.setAddress(new Address("seoul", "1001", "11-22"));
-            member.setPeriod(new Period());
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+            member.getFavoriteFoods().add("치킨");
+
+            member.getAddressHistory().add(new Address("15","22","33"));
+            member.getAddressHistory().add(new Address("15","226","336"));
 
             em.persist(member);
+
+            Member member1 = em.find(Member.class, member.getId());
+            //homeCity -> newCity
+            Address homeAddress = member1.getHomeAddress();
+            //치킨 -> 한식
+            member1.getFavoriteFoods().remove("치킨");
+            member1.getFavoriteFoods().add("한식");
+
+            member1.getAddressHistory().remove(new Address("15","22","33"));
+            member1.getAddressHistory().add(new Address("new city", "22", "33"));
+
+
 
             tx.commit();
         }catch (Exception e){
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,9 +3,7 @@ package hellojpa;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Entity
 public class Member{
@@ -30,7 +28,7 @@ public class Member{
     private List<Product> products = new ArrayList<>();
 
     @Embedded
-    private Address address;
+    private Address homeAddress;
 
     @Embedded
     @AttributeOverrides({
@@ -40,15 +38,54 @@ public class Member{
     })
     private Address workAddress;
 
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD",
+            joinColumns = @JoinColumn(name = "MEMBER_ID"))
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+//    @ElementCollection
+//    @CollectionTable(name = "ADDRESS",
+//            joinColumns = @JoinColumn(name = "MEMBER_ID"))
+//    private List<Address> addressHistory = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
+
     @Embedded
     private Period period;
 
     public Address getAddress() {
-        return address;
+        return homeAddress;
     }
 
-    public void setAddress(Address address) {
-        this.address = address;
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<Address> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<Address> addressHistory) {
+        this.addressHistory = addressHistory;
+    }
+
+    public void setAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
     }
 
     public Period getPeriod() {
